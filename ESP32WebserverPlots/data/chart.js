d02583a27666ad603b3c7036647baeedd96c259a
Igeef <bryan.indarto@ui.ac.id>
2023-11-05 20:31:59 +0700

    Value bar completed

--- ESP32WebserverPlots/data/chart.js
+++ ESP32WebserverPlots/data/chart.js
@@ -1,6 +1,8 @@
 var i = 0;
+var width_ph_bar = 0;
+var width_ntu_bar = 0;
+var width_tds_bar = 0;
 
-// Map function to convert the range
 function mapRange(value, inMin, inMax, outMin, outMax) {
 	return ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
  }
@@ -8,20 +10,72 @@ function mapRange(value, inMin, inMax, outMin, outMax) {
 function adjust_bar() {
   if (i == 0) {
     i = 1;
-    var elem = document.getElementById("pHBar");
-    var width = 0;
-	id = setInterval(bar_adjust, 10);
-	// var pHValue = Math.floor((Math.random() * 14)); // Harus diganti dengan data pH Asli
-	var pHValue = document.getElementById('ph_value').innerHTML;
-	var value = mapRange(pHValue, 0, 14, 0, 100);
-	function bar_adjust(){
-		if(width >= value){
+    var elem1 = document.getElementById("pHBar");
+	var elem2 = document.getElementById("NTUBar");
+	var elem3 = document.getElementById("TDSBar");
+
+	var pHValue = Math.floor((Math.random() * 14));
+	var NTUValue = Math.floor((Math.random() * 3000));
+	var TDSValue = Math.floor((Math.random() * 1000));
+
+	document.getElementById('ph_value').innerHTML = pHValue;
+	document.getElementById('ntu_value').innerHTML = NTUValue;
+	document.getElementById('tds_value').innerHTML = TDSValue;
+
+	// var pHValue = document.getElementById('ph_value').innerHTML;
+	// var NTUValue = document.getElementById('ntu_value').innerHTML;
+	// var TDSValue = document.getElementById('tds_value').innerHTML;
+
+	var value = Math.floor(mapRange(pHValue, 0, 14, 0, 100)) - 5;
+	var value2 = Math.floor(mapRange(NTUValue, 0, 3000, 0, 100)) - 5;
+	var value3 = Math.floor(mapRange(TDSValue, 0, 1000, 0, 100)) - 5;
+
+	id = setInterval(bar_ph_adjust, 10);
+	id2 = setInterval(bar_ntu_adjust, 10);
+	id3 = setInterval(bar_tds_adjust, 10);
+
+	function bar_ph_adjust(){
+		if(width_ph_bar == value){
 			i = 0;
 			clearInterval(id);
 		}
-		else{
-			width++;
-			elem.style.width = width + "%";
+		else if(width_ph_bar < value){
+			width_ph_bar++;
+			elem1.style.width = width_ph_bar + "%";
+		}
+		else if(width_ph_bar > value){
+			width_ph_bar--;
+			elem1.style.width = width_ph_bar + "%";
+		}
+	}
+
+	function bar_ntu_adjust(){
+		if(width_ntu_bar == value2){
+			i = 0;
+			clearInterval(id2);
+		}
+		else if(width_ntu_bar < value2){
+			width_ntu_bar++;
+			elem2.style.width = width_ntu_bar + "%";
+		}
+		else if(width_ntu_bar > value2){
+			width_ntu_bar--;
+			elem2.style.width = width_ntu_bar + "%";
+		}
+	}
+
+	function bar_tds_adjust(){
+		if(width_tds_bar == value3){
+			i = 0;
+			clearInterval(id3);
+		}
+		else if(width_tds_bar < value3){
+			width_tds_bar++;
+			elem3.style.width = width_tds_bar + "%";
+		}
+		else if(width_tds_bar > value2){
+			width_tds_bar--;
+			elem3.style.width = width_tds_bar + "%";
 		}
 	}
   }
